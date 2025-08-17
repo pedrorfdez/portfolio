7847d16214547d1b888cc34e51847d78e81ada7c
pedrorfdez <pfrascon24@gmail.com>
2025-08-17 08:55:59 +0200

    Added ahrefs analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,6 +31,12 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+
+        <Script
+          src='https://analytics.ahrefs.com/analytics.js'
+          data-key='m8giLyOrCMXcwI8osJZTDA'
+          async
+        />
       </body>
     </html>
   );
